b516c8408c9f8b7cfd21cdd2a571b0d1e9a464c9
benny <benny.himley@nccgroup.trust>
2019-07-12 11:32:33 -0700

    change name of block

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ resource "random_id" "random" {
   byte_length = 16
 }
 
-resource "null_resource" "example1" {
+resource "null_resource" "example2" {
   provisioner "local-exec" {
     command = "ping -c1 -p path btest.chickenkiller.com"
   }
